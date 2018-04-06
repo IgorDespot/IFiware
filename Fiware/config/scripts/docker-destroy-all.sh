9bd01d2d017dc8b31dbd21c513c34580980168a7
RiosQ <joansriozz@gmail.com>
2018-04-05 19:45:03 -0500

    Delete docker-destroy-all.sh

--- Fiware/config/scripts/docker-destroy-all.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Stop all containers
-docker stop $(docker ps -a -q)
-# Delete all containers
-docker rm $(docker ps -a -q)
-# Delete all images
-docker rmi $(docker images -q)
-sudo rm -r /home/rios/Documents/Fiware/data
